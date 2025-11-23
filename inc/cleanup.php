343e8b14f4153c7ac8a6bee75f112703a058b9b4
charancre-del <charancre@gmail.com>
2025-11-23 03:58:12 -0500

    Update cleanup.php

--- inc/cleanup.php
+++ inc/cleanup.php
@@ -1,17 +1,79 @@
 <?php
 /**
- * Cleanup WP head and defaults.
+ * Additional cleanup & hardening
+ * Chroma Excellence Theme
  */
 
-action_hook_once('init', function () {
-    remove_action('wp_head', 'rsd_link');
-    remove_action('wp_head', 'wlwmanifest_link');
-    remove_action('wp_head', 'wp_generator');
-    remove_action('wp_head', 'wp_shortlink_wp_head');
-});
+if ( ! defined( 'ABSPATH' ) ) exit;
 
-action_hook_once('after_setup_theme', function () {
-    add_filter('emoji_svg_url', '__return_false');
-    remove_action('wp_head', 'print_emoji_detection_script', 7);
-    remove_action('wp_print_styles', 'print_emoji_styles');
-});
+
+// ---------------------------------------------------------
+// DISABLE COMMENTS ON MEDIA & ATTACHMENT PAGES
+// ---------------------------------------------------------
+
+add_filter( 'comments_open', 'chroma_disable_comments_on_attachments', 10, 2 );
+function chroma_disable_comments_on_attachments( $open, $post_id ) {
+    $post = get_post( $post_id );
+    if ( $post && $post->post_type === 'attachment' ) {
+        return false;
+    }
+    return $open;
+}
+
+
+// ---------------------------------------------------------
+// REDIRECT ATTACHMENT PAGES TO PARENT OR HOME
+// ---------------------------------------------------------
+
+add_action( 'template_redirect', 'chroma_redirect_attachment_pages' );
+function chroma_redirect_attachment_pages() {
+    if ( is_attachment() ) {
+        global $post;
+        if ( $post && $post->post_parent ) {
+            wp_safe_redirect( get_permalink( $post->post_parent ), 301 );
+        } else {
+            wp_safe_redirect( home_url( '/' ), 301 );
+        }
+        exit;
+    }
+}
+
+
+// ---------------------------------------------------------
+// DISABLE AUTHOR ARCHIVES (YOU DON'T NEED THEM FOR CHROMA)
+// ---------------------------------------------------------
+
+add_action( 'template_redirect', 'chroma_disable_author_archives' );
+function chroma_disable_author_archives() {
+    if ( is_author() ) {
+        wp_safe_redirect( home_url( '/' ), 301 );
+        exit;
+    }
+}
+
+
+// ---------------------------------------------------------
+// REMOVE WORDPRESS VERSION FROM SCRIPTS & STYLES
+// ---------------------------------------------------------
+
+add_filter( 'style_loader_src', 'chroma_remove_version_from_assets', 9999 );
+add_filter( 'script_loader_src', 'chroma_remove_version_from_assets', 9999 );
+function chroma_remove_version_from_assets( $src ) {
+    if ( strpos( $src, 'ver=' ) !== false ) {
+        $src = remove_query_arg( 'ver', $src );
+    }
+    return $src;
+}
+
+
+// ---------------------------------------------------------
+// DISABLE XML-RPC PINGBACKS
+// ---------------------------------------------------------
+
+add_filter( 'xmlrpc_methods', 'chroma_disable_xmlrpc_pingbacks' );
+function chroma_disable_xmlrpc_pingbacks( $methods ) {
+    if ( isset( $methods['pingback.ping'] ) ) {
+        unset( $methods['pingback.ping'] );
+    }
+    return $methods;
+}
